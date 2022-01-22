9363f6021a51e3fd386e0f14ad4e29da74c10b7a
KonstantinKhan <khan366kos@gmail.com>
2022-01-22 22:14:29 +0500

    Updated spec-fitChanges-api-product.yaml:  - added format Created SerializationTest.kt for serialization and deserialization.

---
+++ fitChanges-be-transport-openapi/src/test/kotlin/SerializationTest.kt
@@ -0,0 +1,52 @@
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import ru.fitChanges.openapi.models.BaseMessage
+import ru.fitChanges.openapi.models.CreatableProduct
+import ru.fitChanges.openapi.models.CreateProductRequest
+import ru.fitChanges.openapi.models.ResponseProduct
+import kotlin.test.Test
+import kotlin.test.assertContains
+import kotlin.test.assertEquals
+
+class SerializationTest {
+
+    private val requestId = "rID:0001"
+
+    // Test example of a ResponseProduct
+    private val beef = ResponseProduct(
+        productName = "Говядина",
+        caloriesPerHundredGrams = 187.0,
+        proteinsPerHundredGrams = 18.9,
+        fatsPerHundredGrams = 12.4,
+        carbohydratePerHundredGrams = 0.0
+    )
+
+    // Test example of a CreateProductRequest
+    private val createBeef = CreateProductRequest(
+        requestId = requestId,
+        createProduct = CreatableProduct(
+            productName = "Говядина",
+            caloriesPerHundredGrams = 187.0,
+            proteinsPerHundredGrams = 18.9,
+            fatsPerHundredGrams = 12.4,
+            carbohydratePerHundredGrams = 0.0
+        )
+    )
+
+    private val om = ObjectMapper()
+
+    private val json = om.writeValueAsString(createBeef)
+
+    @Test
+    fun serializationProductTest() {
+        println(json)
+        assertContains(json, Regex("messageType\":\\s*\"${CreateProductRequest::class.simpleName}"))
+    }
+
+    @Test
+    fun deserializationProductTest() {
+        val deserialized = om.readValue(json, BaseMessage::class.java) as CreateProductRequest
+        println(deserialized)
+        assertEquals(requestId, deserialized.requestId)
+    }
+}
