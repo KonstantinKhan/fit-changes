9e9ce9c0bfd764e92f3edec9333220dd76b9f31f
KonstantinKhan <khan366kos@gmail.com>
2022-01-26 22:42:07 +0500

    Added kafka route.

--- test_kafka/src/main/kotlin/ru/fit_changes/kafka_test/Application.kt
+++ test_kafka/src/main/kotlin/ru/fit_changes/kafka_test/Application.kt
@@ -1,16 +1,21 @@
-package ru.fitChanges.backend.ktor_product
+package ru.fit_changes.kafka_test
 
 import io.ktor.server.application.*
 import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import ru.fit_changes.kafka_test.plugins.kafka
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
-fun Application.module(testing: Boolean = false) {
+fun Application.module() {
     routing {
+
+        kafka()
+
         get("/") {
-            call.respondText("Hello, world!")
+            call.respondText("Hello, World")
         }
+
     }
 }
