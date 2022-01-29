4a7380c11cfcca97db3b9024baf69bc58c7c7f87
KonstantinKhan <khan366kos@gmail.com>
2022-01-30 02:55:37 +0500

    Created ProductCrudTest.kt.

---
+++ fit_changes-be-product-logics/src/test/kotlin/ru/fit_changes/backend/product/logics/ProductCrudTest.kt
@@ -0,0 +1,37 @@
+package ru.fit_changes.backend.product.logics
+
+import kotlinx.coroutines.runBlocking
+import ru.fit_changes.backend.common.context.BeContext
+import ru.fitChanges.backend.utils.product.BEEF_FILLED_MODEL
+import ru.fitChanges.backend.utils.product.BEEF_FILLED_MODEL_WITHOUT_PRODUCT_ID
+import ru.fitChanges.backend.utils.product.REQUEST_ID_0001
+import ru.fit_changes.backend.common.context.Operations
+import ru.fit_changes.backend.common.models.StubCases
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ProductCrudTest {
+
+    @Test
+    fun productCreateSuccess() {
+        val crud = ProductCrud()
+        val context = BeContext(
+            requestId = REQUEST_ID_0001,
+            requestProduct = BEEF_FILLED_MODEL_WITHOUT_PRODUCT_ID,
+            operation = Operations.CREATE,
+            stubCase = StubCases.SUCCESS
+        )
+        runBlocking {
+            crud.create(context)
+            val expected = BEEF_FILLED_MODEL
+            with(context.responseProduct) {
+                assertEquals(expected.productName, productName)
+                assertEquals(expected.caloriesPerHundredGrams, caloriesPerHundredGrams)
+                assertEquals(expected.proteinsPerHundredGrams, proteinsPerHundredGrams)
+                assertEquals(expected.fatsPerHundredGrams, fatsPerHundredGrams)
+                assertEquals(expected.carbohydratePerHundredGrams, carbohydratePerHundredGrams)
+                assertEquals(expected.productId, productId)
+            }
+        }
+    }
+}
