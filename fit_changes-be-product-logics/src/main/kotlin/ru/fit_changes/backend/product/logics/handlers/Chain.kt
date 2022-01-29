de9a47f2640949dcfeaf39a67c0b53575f2d32db
KonstantinKhan <khan366kos@gmail.com>
2022-01-30 02:47:06 +0500

    Created Chain.kt with classes CorChainDsl and CorChain.

---
+++ fit_changes-be-product-logics/src/main/kotlin/ru/fit_changes/backend/product/logics/handlers/Chain.kt
@@ -0,0 +1,36 @@
+package ru.fit_changes.backend.product.logics.handlers
+
+import ru.fit_changes.backend.product.logics.*
+
+class CorChainDsl<T>(
+    private val builders: MutableList<ICorExecutorBuilder<T>> = mutableListOf(),
+    override var title: String = "",
+    override var description: String = ""
+
+) : ICorChain<T>, CorComponentDsl<T>() {
+    override fun add(builder: ICorExecutorBuilder<T>) {
+        builders.add(builder)
+    }
+
+    override fun build(): ICorExecutor<T> = CorChain(
+        title = title,
+        description = description,
+        executors = builders.map { it.build() },
+        blockOn = blockOn,
+        blockExcept = blockExcept
+
+    )
+}
+
+class CorChain<T>(
+    private val executors: List<ICorExecutor<T>>,
+    override val title: String,
+    override val description: String,
+    override val blockOn: T.() -> Boolean,
+    override val blockExcept: T.(t: Throwable) -> Unit
+
+) : AbstractWorker<T>(blockOn, blockExcept) {
+    override suspend fun work(context: T) {
+        executors.forEach { it.exec(context) }
+    }
+}
