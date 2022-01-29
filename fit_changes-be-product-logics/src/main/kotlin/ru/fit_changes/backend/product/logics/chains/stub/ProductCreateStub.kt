bd3f7a13f5ca95cf62a162f7f6878ec963a04e53
KonstantinKhan <khan366kos@gmail.com>
2022-01-30 02:54:01 +0500

    Created ProductCreateStub.kt

---
+++ fit_changes-be-product-logics/src/main/kotlin/ru/fit_changes/backend/product/logics/chains/stub/ProductCreateStub.kt
@@ -0,0 +1,20 @@
+package ru.fit_changes.backend.product.logics.chains.stub
+
+import ru.fitChanges.backend.utils.product.PRODUCT_ID_0001
+import ru.fit_changes.backend.common.context.BeContext
+import ru.fit_changes.backend.common.context.CorStatus
+import ru.fit_changes.backend.common.models.StubCases
+import ru.fit_changes.backend.common.product.models.ProductIdModel
+import ru.fit_changes.backend.product.logics.handlers.CorChainDsl
+import ru.fit_changes.backend.product.logics.handlers.addCorWorkerDsl
+
+fun CorChainDsl<BeContext>.productCreateStub(title: String) = addCorWorkerDsl {
+    this.title = title
+    on {
+        status == CorStatus.RUNNING &&
+                stubCase == StubCases.SUCCESS
+    }
+    handle {
+        responseProduct = requestProduct.copy(productId = ProductIdModel(PRODUCT_ID_0001))
+    }
+}
