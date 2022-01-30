d7354670672d83744c8e6a99879920a72f968b2e
KonstantinKhan <khan366kos@gmail.com>
2022-01-30 21:49:21 +0500

    Added constructor with UUID to ProductIdModel.kt.

--- fit_changes-be-common/src/main/kotlin/ru/fit_changes/backend/common/product/models/ProductIdModel.kt
+++ fit_changes-be-common/src/main/kotlin/ru/fit_changes/backend/common/product/models/ProductIdModel.kt
@@ -1,7 +1,12 @@
 package ru.fit_changes.backend.common.product.models
 
+import java.util.*
+
 @JvmInline
 value class ProductIdModel(private val id: String) {
+
+    constructor(id: UUID) : this(id.toString())
+
     companion object {
         val NONE = ProductIdModel("")
     }
