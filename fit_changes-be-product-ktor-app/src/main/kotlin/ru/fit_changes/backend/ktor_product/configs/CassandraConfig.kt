e047f5b6648c850cf2eca5e34c9da5bf779a448e
KonstantinKhan <khan366kos@gmail.com>
2022-01-30 21:46:14 +0500

    Added Cassandra config from otus project.

---
+++ fit_changes-be-product-ktor-app/src/main/kotlin/ru/fit_changes/backend/ktor_product/configs/CassandraConfig.kt
@@ -0,0 +1,26 @@
+package ru.fit_changes.backend.ktor_product.configs
+
+import io.ktor.server.application.*
+
+// not yet used anywhere
+// as an example
+
+data class CassandraConfig(
+    val hosts: String = "localhost",
+    val port: Int = 9042,
+    val user: String = "cassandra",
+    val pass: String = "cassandra",
+    val keyspace: String = "test_keyspace"
+) {
+    constructor(environment: ApplicationEnvironment) : this(
+        hosts = environment.config.property("$PATH.hosts").getString(),
+        port = environment.config.property("$PATH.port").getString().toInt(),
+        user = environment.config.property("$PATH.user").getString(),
+        pass = environment.config.property("$PATH.pass").getString(),
+        keyspace = environment.config.property("$PATH.keyspace").getString()
+    )
+
+    companion object {
+        const val PATH = "ktor.repository.cassandra"
+    }
+}
