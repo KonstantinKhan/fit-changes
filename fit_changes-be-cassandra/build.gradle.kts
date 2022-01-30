51301a95b492dc38148836b2bf270c91d2d787b0
KonstantinKhan <khan366kos@gmail.com>
2022-01-30 18:39:14 +0500

    Added implementations.

--- fit_changes-be-cassandra/build.gradle.kts
+++ fit_changes-be-cassandra/build.gradle.kts
@@ -1,7 +1,19 @@
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
+
 plugins {
     kotlin("jvm")
+    kotlin("kapt")
 }
 
 dependencies {
+    val coroutinesVersion: String by project
+    val cassandraDriverVersion: String by project
+
     implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+
+    implementation("com.datastax.oss:java-driver-core:$cassandraDriverVersion")
+    implementation("com.datastax.oss:java-driver-query-builder:$cassandraDriverVersion")
+    kapt("com.datastax.oss:java-driver-mapper-processor:$cassandraDriverVersion")
+    implementation("com.datastax.oss:java-driver-mapper-runtime:$cassandraDriverVersion")
 }
