a8be605c6cd1d6afcc2e7a7540c2533ede8ff244
KonstantinKhan <khan366kos@gmail.com>
2022-02-03 22:48:53 +0500

    Added function handleRequest into ProductService.kt.

--- fit_changes-be-product-service/src/main/kotlin/ru/fit_chages/backend/product/service/ProductService.kt
+++ fit_changes-be-product-service/src/main/kotlin/ru/fit_chages/backend/product/service/ProductService.kt
@@ -2,6 +2,7 @@ package ru.fit_chages.backend.product.service
 
 import ru.fitChanges.backend.mapping.product.setQuery
 import ru.fitChanges.backend.mapping.product.toCreateProductResponse
+import ru.fitChanges.openapi.models.BaseMessage
 import ru.fitChanges.openapi.models.CreateProductRequest
 import ru.fitChanges.openapi.models.CreateProductResponse
 import ru.fit_changes.backend.common.context.BeContext
@@ -10,9 +11,18 @@ import ru.fit_changes.backend.product.logics.ProductCrud
 class ProductService(
     private var crud: ProductCrud
 ) {
+
+    suspend fun handleRequest(context: BeContext, request: BaseMessage) = try {
+        when (request) {
+            is CreateProductRequest -> createProduct(context, request)
+            else -> throw DataNotAllowedException("Request is not allowed", request)
+        }
+    } catch (t: Throwable) {
+        println(t.message)
+    }
+
     suspend fun createProduct(context: BeContext, request: CreateProductRequest): CreateProductResponse {
         context.setQuery(request)
-        context.responseProduct = context.requestProduct
         return context.toCreateProductResponse()
     }
 }
