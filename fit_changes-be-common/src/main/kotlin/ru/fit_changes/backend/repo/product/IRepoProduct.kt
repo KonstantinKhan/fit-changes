dbd1b12055ade6f38382f395d0f1841d247a906f
KonstantinKhan <khan366kos@gmail.com>
2022-02-03 22:48:18 +0500

    Added NONE into IRepoProduct.kt

--- fit_changes-be-common/src/main/kotlin/ru/fit_changes/backend/repo/product/IRepoProduct.kt
+++ fit_changes-be-common/src/main/kotlin/ru/fit_changes/backend/repo/product/IRepoProduct.kt
@@ -6,4 +6,26 @@ interface IRepoProduct {
     suspend fun update(req: DbProductModelRequest): DbProductResponse
     suspend fun delete(req: DbProductIdRequest): DbProductResponse
     suspend fun search(req: DbProductFilterRequest): DbProductsResponse
+
+    object NONE: IRepoProduct {
+        override suspend fun create(req: DbProductModelRequest): DbProductResponse {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun read(req: DbProductIdRequest): DbProductResponse {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun update(req: DbProductModelRequest): DbProductResponse {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun delete(req: DbProductIdRequest): DbProductResponse {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun search(req: DbProductFilterRequest): DbProductsResponse {
+            TODO("Not yet implemented")
+        }
+    }
 }
