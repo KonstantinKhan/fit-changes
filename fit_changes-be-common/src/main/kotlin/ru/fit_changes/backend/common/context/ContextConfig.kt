16f1e6c17b9634d4a167ec6ee846189915b34ad5
KonstantinKhan <khan366kos@gmail.com>
2022-02-03 22:47:21 +0500

    Added variables in ContextConfig.kt

--- fit_changes-be-common/src/main/kotlin/ru/fit_changes/backend/common/context/ContextConfig.kt
+++ fit_changes-be-common/src/main/kotlin/ru/fit_changes/backend/common/context/ContextConfig.kt
@@ -1,5 +1,8 @@
 package ru.fit_changes.backend.common.context
 
+import ru.fit_changes.backend.repo.product.IRepoProduct
+
 data class ContextConfig(
-    val tempStub: String = ""
+    val repoProductTest: IRepoProduct = IRepoProduct.NONE,
+    val repoProductProd: IRepoProduct = IRepoProduct.NONE,
 )
