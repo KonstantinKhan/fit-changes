4aa91fff03405d2401cbf5bdbe94cc7d828ba698
KonstantinKhan <khan366kos@gmail.com>
2022-02-05 21:31:21 +0500

    Updated debug.

--- fit_changes-be-mapping/src/main/kotlin/ru/fitChanges/backend/mapping/product/Transport2Context.kt
+++ fit_changes-be-mapping/src/main/kotlin/ru/fitChanges/backend/mapping/product/Transport2Context.kt
@@ -1,22 +1,31 @@
 package ru.fitChanges.backend.mapping.product
 
+import ru.fitChanges.openapi.models.BaseDebugRequest
 import ru.fit_changes.backend.common.context.BeContext
 import ru.fit_changes.backend.common.product.models.ProductModel
 import ru.fitChanges.openapi.models.CreatableProduct
 import ru.fitChanges.openapi.models.CreateProductRequest
+import ru.fit_changes.backend.common.models.StubCases
 
 fun BeContext.setQuery(query: CreateProductRequest) = apply {
     requestId = query.requestId ?: ""
     requestProduct = query.createProduct?.toModel(this) ?: ProductModel()
+    stubCase = query.debug?.stubCase.toModel()
 }
 
-private fun CreatableProduct.toModel(beContext: BeContext) = ProductModel(
+private fun CreatableProduct.toModel(context: BeContext) = ProductModel(
     productName = productName ?: "",
-    caloriesPerHundredGrams = caloriesPerHundredGrams.validationProductParameters(beContext, "Calories"),
-    proteinsPerHundredGrams = proteinsPerHundredGrams.validationProductParameters(beContext, "Proteins"),
-    fatsPerHundredGrams = fatsPerHundredGrams.validationProductParameters(beContext, "Fats"),
-    carbohydratesPerHundredGrams = carbohydratesPerHundredGrams.validationProductParameters(beContext, "Carbohydrates")
+    caloriesPerHundredGrams = caloriesPerHundredGrams.validationProductParameters(context, "Calories"),
+    proteinsPerHundredGrams = proteinsPerHundredGrams.validationProductParameters(context, "Proteins"),
+    fatsPerHundredGrams = fatsPerHundredGrams.validationProductParameters(context, "Fats"),
+    carbohydratesPerHundredGrams = carbohydratesPerHundredGrams.validationProductParameters(context, "Carbohydrates")
 )
+
+private fun BaseDebugRequest.StubCase?.toModel(): StubCases = when (this) {
+    BaseDebugRequest.StubCase.SUCCESS -> StubCases.SUCCESS
+    BaseDebugRequest.StubCase.DATABASE_ERROR -> StubCases.DATABASE_ERROR
+    null -> StubCases.NONE
+}
 
 private fun Double?.validationProductParameters(context: BeContext, parameter: String) =
     if (this != null) {
