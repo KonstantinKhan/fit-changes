acaff16b143ba270c966c1a13ef030d2de850712
KonstantinKhan <khan366kos@gmail.com>
2022-02-10 23:09:07 +0500

    Added repoCreate.

--- fit_changes-be-product-logics/src/main/kotlin/ru/fit_changes/backend/product/logics/chains/product/ProductCreate.kt
+++ fit_changes-be-product-logics/src/main/kotlin/ru/fit_changes/backend/product/logics/chains/product/ProductCreate.kt
@@ -5,8 +5,10 @@ import ru.fit_changes.backend.product.logics.ICorExecutor
 import ru.fit_changes.backend.product.logics.chain
 import ru.fit_changes.backend.product.logics.chains.stub.productCreateStub
 import ru.fit_changes.backend.product.logics.workers.chainInitWorker
+import ru.fit_changes.backend.product.logics.workers.repoCreate
 
 object ProductCreate : ICorExecutor<BeContext> by chain<BeContext>({
     chainInitWorker("Initialization of chain")
     productCreateStub("Processing of stub case CREATE")
+    repoCreate("Write to DB")
 }).build()
