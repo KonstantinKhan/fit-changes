9fca104828313452473b3dc1b6e771d5eaba0247
KonstantinKhan <khan366kos@gmail.com>
2022-02-20 15:54:16 +0500

    Added test request.

--- fit_changes-client-angular/src/app/app.component.ts
+++ fit_changes-client-angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(private http: HttpClient) {
+  }
+
   title = 'fit_changes-client-angular';
+
+  ngOnInit() {
+    this.http.get('http://localhost:8080/', {responseType: 'text'})
+      .subscribe(response => {
+        console.log(response)
+      })
+  }
 }
