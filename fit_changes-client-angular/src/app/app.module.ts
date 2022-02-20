93bf54e80638bd3a48fd06fcfe90de34792b832c
KonstantinKhan <khan366kos@gmail.com>
2022-02-20 16:12:41 +0500

    Added MainLayoutComponent and HomePageComponent.

--- fit_changes-client-angular/src/app/app.module.ts
+++ fit_changes-client-angular/src/app/app.module.ts
@@ -4,10 +4,14 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
