a3f64dd25b701aebdf1eea4ecf25cdab533bc040
KonstantinKhan <khan366kos@gmail.com>
2022-02-20 19:54:01 +0500

    Added lazy load of admin module.

--- fit_changes-client-angular/src/app/app-routing.module.ts
+++ fit_changes-client-angular/src/app/app-routing.module.ts
@@ -9,6 +9,11 @@ const routes: Routes = [
       {path: '', redirectTo: '/', pathMatch: 'full'},
       {path: '', component: HomePageComponent}
     ]
+  },
+  {
+    path: 'admin', loadChildren: () => import('./admin/admin.module').then(m =>
+      m.AdminModule
+    )
   }
 ];
 
