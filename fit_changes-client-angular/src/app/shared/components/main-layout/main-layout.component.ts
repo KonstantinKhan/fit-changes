81735dd9f6900a8b7fd86e29ce6b88d5691560e6
KonstantinKhan <khan366kos@gmail.com>
2022-03-07 21:43:56 +0500

    Updated main layout component.

--- fit_changes-client-angular/src/app/shared/components/main-layout/main-layout.component.ts
+++ fit_changes-client-angular/src/app/shared/components/main-layout/main-layout.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit, ViewEncapsulation} from '@angular/core';
+import {MatDrawerMode} from "@angular/material/sidenav";
 
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
-  styleUrls: ['./main-layout.component.scss']
+  styleUrls: ['./main-layout.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class MainLayoutComponent implements OnInit {
 
-  constructor() { }
+  opacityElement: string = '0.6';
+  widthWindow: number = window.innerWidth;
+  isMenuOpen: boolean;
+  isFullPanel = true;
+  isFocusSidenav = false;
 
-  ngOnInit(): void {
+  constructor() {
+    this.isMenuOpen = false
   }
 
+  ngOnInit(): void {
+    this.positionSideNav()
+  }
+
+  positionSideNav() {
+    this.isMenuOpen = this.isLargeScreen();
+  }
+
+  isLargeScreen(): boolean {
+    return this.widthWindow >= 600;
+  }
+
+  getSidenavMode(): MatDrawerMode {
+    if (this.isLargeScreen()) {
+      return 'side';
+    } else return 'over';
+  }
+
+  onToolBarMenuToggle() {
+    this.isFullPanel = !this.isFullPanel;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  getWidthWindow(event: Event) {
+    this.widthWindow = (<Window>event.target).innerWidth;
+    this.positionSideNav()
+  }
 }
