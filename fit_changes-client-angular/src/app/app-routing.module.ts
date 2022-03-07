c7f90f0b05b738d77af021e3d247240c3b4c8ffe
KonstantinKhan <khan366kos@gmail.com>
2022-03-07 21:42:57 +0500

    Added routing.

--- fit_changes-client-angular/src/app/app-routing.module.ts
+++ fit_changes-client-angular/src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import {NgModule} from '@angular/core';
 import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 import {MainLayoutComponent} from "./shared/components/main-layout/main-layout.component";
 import {HomePageComponent} from "./home-page/home-page.component";
+import {ProductsPageComponent} from "./products-page/products-page.component";
+import {DishesPageComponent} from "./dishes-page/dishes-page.component";
 
 const routes: Routes = [
   {
     path: '', component: MainLayoutComponent, children: [
       {path: '', redirectTo: 'home', pathMatch: 'full'},
-      {path: 'home', component: HomePageComponent}
+      {path: 'home', component: HomePageComponent},
+      {path: 'products', component: ProductsPageComponent},
+      {path: 'dishes', component: DishesPageComponent}
     ]
   },
   {
