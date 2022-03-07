271038cb7454be683700a0fef06b327da941ebbe
KonstantinKhan <khan366kos@gmail.com>
2022-03-07 21:40:59 +0500

    Created dishes page.

---
+++ fit_changes-client-angular/src/app/dishes-page/dishes-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dishes-page',
+  templateUrl: './dishes-page.component.html',
+  styleUrls: ['./dishes-page.component.scss']
+})
+export class DishesPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
