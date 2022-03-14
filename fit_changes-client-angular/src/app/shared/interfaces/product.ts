0500a2624f0fd727c79a310a6b8036eb03aee5c5
KonstantinKhan <khan366kos@gmail.com>
2022-03-14 21:40:30 +0500

    Created product interface.

---
+++ fit_changes-client-angular/src/app/shared/interfaces/product.ts
@@ -0,0 +1,8 @@
+export interface Product {
+  id?: string
+  productName: string
+  caloriesPerHundredGrams: number
+  proteinsPerHundredGrams: number
+  fatsPerHundredGrams: number
+  carbohydratesPerHundredGrams: number
+}
