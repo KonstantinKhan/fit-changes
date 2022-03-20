a6231920c33c81583e1877680f03b2b12058f1bb
KonstantinKhan <khan366kos@gmail.com>
2022-03-20 14:37:46 +0500

    Updated search-input component.

--- fit_changes-client-angular/src/app/shared/components/search-input/search-input.component.ts
+++ fit_changes-client-angular/src/app/shared/components/search-input/search-input.component.ts
@@ -1,31 +1,105 @@
-import {Component, OnInit} from '@angular/core';
+import {
+  Component,
+  ElementRef, HostListener,
+  OnInit, Renderer2,
+  ViewChild,
+  ViewEncapsulation
+} from '@angular/core';
 import {FormControl} from "@angular/forms";
-import {map, Observable, startWith} from "rxjs";
+import {ProductSearchService} from "../../../services/product-search.service";
+import {MatFormField} from "@angular/material/form-field";
+import {Product} from "../../interfaces/product";
 
 @Component({
   selector: 'app-search-input',
   templateUrl: './search-input.component.html',
-  styleUrls: ['./search-input.component.scss']
+  styleUrls: ['./search-input.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SearchInputComponent implements OnInit {
-  value: string = "";
-  searchControl = new FormControl();
-  products: string[] = ['Помидор', 'Говядина', 'Молоко'];
-  filteredProducts?: Observable<string[]>;
 
-  ngOnInit() {
-    this.filteredProducts = this.searchControl.valueChanges.pipe(
-      startWith(''),
-      map(value => this._filter(value)),
-    );
+  @ViewChild('search_results') searchResults!: ElementRef
+  @ViewChild('search_input') searchInput!: MatFormField
+
+  query: string = "";
+  searchControl = new FormControl();
+
+  products: Product[] = []
+
+  testProducts: Product[] = [
+    {
+      id: '0001',
+      productName: "Филе куриное",
+      caloriesPerHundredGrams: 110.0,
+      proteinsPerHundredGrams: 21.0,
+      fatsPerHundredGrams: 2.5,
+      carbohydratesPerHundredGrams: 0.0
+    },
+    {
+      id: '0002',
+      productName: "Помидор",
+      caloriesPerHundredGrams: 20.0,
+      proteinsPerHundredGrams: 1.1,
+      fatsPerHundredGrams: 0.2,
+      carbohydratesPerHundredGrams: 3.7
+    },
+    {
+      id: '0003',
+      productName: "Гречка",
+      caloriesPerHundredGrams: 313.0,
+      proteinsPerHundredGrams: 12.6,
+      fatsPerHundredGrams: 3.3,
+      carbohydratesPerHundredGrams: 62.1
+    },
+  ]
+
+  searchWidth = ''
+  isRender = false;
+
+  constructor(
+    private searchService: ProductSearchService,
+    private renderer: Renderer2
+  ) {
   }
 
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-    if (!filterValue.trim()) {
-      return []
+  ngOnInit() {
+    this.products = this.testProducts
+  }
+
+  ngAfterViewInit() {
+    this.searchWidth = this.searchInput._elementRef.nativeElement.offsetWidth
+  }
+
+  ngAfterViewChecked() {
+
+    if (this.searchWidth != this.searchInput._elementRef.nativeElement.offsetWidth) {
+      this.searchWidth = this.searchInput._elementRef.nativeElement.offsetWidth
+      if (this.isRender) {
+        this.isRender = false
+      }
+    }
+
+    if (this.searchResults === undefined) {
+      if (this.isRender) {
+        this.isRender = false
+      }
     } else {
-      return this.products.filter(option => option.toLowerCase().includes(filterValue));
+      if (!this.isRender) {
+        this.renderer.setStyle(
+          this.searchResults.nativeElement,
+          'width',
+          `${this.searchWidth}px`
+        )
+        this.isRender = true
+      }
+    }
+  }
+
+  @HostListener('window:resize')
+  changeWidthWindow() {
+    this.searchWidth = this.searchInput._elementRef.nativeElement.offsetWidth
+    if (this.isRender) {
+      this.isRender = false
     }
   }
 }
