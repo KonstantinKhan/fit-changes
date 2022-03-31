333d7bfd5537c3413cbec03a176b0c9523972448
KonstantinKhan <khan366kos@gmail.com>
2022-03-31 20:15:00 +0500

    Created CaloriesModel.kt

---
+++ fit_changes-be-common/src/main/kotlin/ru/fit_changes/backend/common/product/models/CaloriesModel.kt
@@ -0,0 +1,11 @@
+package ru.fit_changes.backend.common.product.models
+
+@JvmInline
+value class CaloriesModel(private val calories: Double) {
+    val value: Double
+        get() = calories
+
+    companion object {
+        val NONE = CaloriesModel(0.0)
+    }
+}
