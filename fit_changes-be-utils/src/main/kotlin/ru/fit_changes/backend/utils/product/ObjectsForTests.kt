19baefb84de33d2d78ebfdfc1b06d9e142fc30fe
KonstantinKhan <khan366kos@gmail.com>
2022-04-02 16:19:16 +0500

    Updated test products.

--- fit_changes-be-utils/src/main/kotlin/ru/fit_changes/backend/utils/product/ObjectsForTests.kt
+++ fit_changes-be-utils/src/main/kotlin/ru/fit_changes/backend/utils/product/ObjectsForTests.kt
@@ -1,8 +1,8 @@
 package ru.fit_changes.backend.utils.product
 
-import ru.fit_changes.backend.common.product.models.ProductModel
 import ru.fitChanges.openapi.models.CreatableProduct
 import ru.fitChanges.openapi.models.UpdatableProduct
+import ru.fit_changes.backend.common.product.models.*
 
 const val REQUEST_ID_0001 = "requestID:0001"
 const val PRODUCT_ID_0001 = "productID:0001"
@@ -42,8 +42,8 @@ val BEEF_NOT_FILLED_UPDATABLE_PRODUCT = BEEF_FILLED_UPDATABLE_PRODUCT.copy(
 
 val BEEF_FILLED_MODEL = ProductModel(
     productName = "Говядина",
-    caloriesPerHundredGrams = 187.0,
-    proteinsPerHundredGrams = 18.9,
-    fatsPerHundredGrams = 12.4,
-    carbohydratesPerHundredGrams = 0.0,
+    caloriesPerHundredGrams = CaloriesModel(187.0),
+    proteinsPerHundredGrams = ProteinsModel(18.9),
+    fatsPerHundredGrams = FatsModel(12.4),
+    carbohydratesPerHundredGrams = CarbohydratesModel(0.0),
 )
