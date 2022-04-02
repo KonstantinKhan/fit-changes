a5b60b6a449064f014dc44f165799baedf3327d2
KonstantinKhan <khan366kos@gmail.com>
2022-04-02 16:14:39 +0500

    Updated ProductModel.kt. Added model for proteins, fats and carbohydrates.

--- fit_changes-be-common/src/main/kotlin/ru/fit_changes/backend/common/product/models/ProductModel.kt
+++ fit_changes-be-common/src/main/kotlin/ru/fit_changes/backend/common/product/models/ProductModel.kt
@@ -1,11 +1,12 @@
 package ru.fit_changes.backend.common.product.models
 
 data class ProductModel(
-    var productName: String = "",
-    var caloriesPerHundredGrams: Double = 0.0,
-    var proteinsPerHundredGrams: Double = 0.0,
-    var fatsPerHundredGrams: Double = 0.0,
-    var carbohydratesPerHundredGrams: Double = 0.0,
     var productId: ProductIdModel = ProductIdModel.NONE,
+    var authorId: AuthorIdModel = AuthorIdModel.NONE,
+    var productName: String = "",
+    var caloriesPerHundredGrams: CaloriesModel = CaloriesModel.NONE,
+    var proteinsPerHundredGrams: ProteinsModel = ProteinsModel.NONE,
+    var fatsPerHundredGrams: FatsModel = FatsModel.NONE,
+    var carbohydratesPerHundredGrams: CarbohydratesModel = CarbohydratesModel.NONE,
     var permissions: MutableSet<ProductPermissions> = mutableSetOf()
 )
