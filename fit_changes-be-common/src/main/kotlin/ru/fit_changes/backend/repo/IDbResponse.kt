5b8f81693746325c1faa4587d126409054f03513
KonstantinKhan <khan366kos@gmail.com>
2022-04-02 16:11:53 +0500

    Updated IDbResponse.kt. Added status and errors.

--- fit_changes-be-common/src/main/kotlin/ru/fit_changes/backend/repo/IDbResponse.kt
+++ fit_changes-be-common/src/main/kotlin/ru/fit_changes/backend/repo/IDbResponse.kt
@@ -1,5 +1,9 @@
 package ru.fit_changes.backend.repo
 
+import ru.fit_changes.backend.common.models.CommonErrorModel
+
 interface IDbResponse<T> {
+    val isSuccess: Boolean
+    val errors: List<CommonErrorModel>
     val result: T
 }
