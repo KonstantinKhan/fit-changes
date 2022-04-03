5c5d6c312e6cd40d845fb2c17b9ea403a7cc36dc
KonstantinKhan <khan366kos@gmail.com>
2022-04-03 13:49:29 +0500

    Added properties and constructors for DbProductResponse data class.

--- fit_changes-be-common/src/main/kotlin/ru/fit_changes/backend/repo/product/DbProductResponse.kt
+++ fit_changes-be-common/src/main/kotlin/ru/fit_changes/backend/repo/product/DbProductResponse.kt
@@ -1,11 +1,14 @@
 package ru.fit_changes.backend.repo.product
 
+import ru.fit_changes.backend.common.models.CommonErrorModel
 import ru.fit_changes.backend.common.product.models.ProductModel
 import ru.fit_changes.backend.repo.IDbResponse
 
 data class DbProductResponse(
-    override val result: ProductModel?
-
+    override val result: ProductModel?,
+    override val isSuccess: Boolean,
+    override val errors: List<CommonErrorModel> = emptyList(),
 ) : IDbResponse<ProductModel?> {
-
+    constructor(result: ProductModel) : this(result, true, emptyList())
+    constructor(error: CommonErrorModel) : this(null, false, listOf(error))
 }
