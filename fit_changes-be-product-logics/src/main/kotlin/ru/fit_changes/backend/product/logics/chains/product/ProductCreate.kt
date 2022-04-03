6b773e9b0c186137ebe69f0cc74e86492cd1268b
KonstantinKhan <khan366kos@gmail.com>
2022-04-03 13:59:04 +0500

    Added chooseDb worker.

--- fit_changes-be-product-logics/src/main/kotlin/ru/fit_changes/backend/product/logics/chains/product/ProductCreate.kt
+++ fit_changes-be-product-logics/src/main/kotlin/ru/fit_changes/backend/product/logics/chains/product/ProductCreate.kt
@@ -5,10 +5,12 @@ import ru.fit_changes.backend.product.logics.ICorExecutor
 import ru.fit_changes.backend.product.logics.chain
 import ru.fit_changes.backend.product.logics.chains.stub.productCreateStub
 import ru.fit_changes.backend.product.logics.workers.chainInitWorker
+import ru.fit_changes.backend.product.logics.workers.chooseDb
 import ru.fit_changes.backend.product.logics.workers.repoCreate
 
 object ProductCreate : ICorExecutor<BeContext> by chain<BeContext>({
     chainInitWorker("Initialization of chain")
+    chooseDb(title = "Choose DB or STUB")
     productCreateStub("Processing of stub case CREATE")
     repoCreate("Write to DB")
 }).build()
