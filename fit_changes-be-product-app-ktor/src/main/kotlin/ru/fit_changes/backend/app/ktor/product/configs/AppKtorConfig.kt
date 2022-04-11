b113747a47e63a0d0161cc79fefff7107a939bd7
KonstantinKhan <khan366kos@gmail.com>
2022-04-11 23:02:01 +0500

    Added repoProductProd in AppKtorConfig.kt.

--- fit_changes-be-product-app-ktor/src/main/kotlin/ru/fit_changes/backend/app/ktor/product/configs/AppKtorConfig.kt
+++ fit_changes-be-product-app-ktor/src/main/kotlin/ru/fit_changes/backend/app/ktor/product/configs/AppKtorConfig.kt
@@ -1,10 +1,17 @@
 package ru.fit_changes.backend.app.ktor.product.configs
 
 import ru.fit_changes.backend.common.context.ContextConfig
+import ru.fit_changes.backend.repo.cassandra.CassandraObject
 import ru.fit_changes.backend.repo.inmemory.RepoProductInMemory
+import ru.fit_changes.backend.repo.product.IRepoProduct
 
 data class AppKtorConfig(
     val contextConfig: ContextConfig = ContextConfig(
-        repoProductTest = RepoProductInMemory()
+        repoProductTest = RepoProductInMemory(),
+        repoProductProd = try {
+            CassandraObject.createRepo()
+        } catch (e: Exception) {
+            IRepoProduct.NONE
+        }
     )
 )
