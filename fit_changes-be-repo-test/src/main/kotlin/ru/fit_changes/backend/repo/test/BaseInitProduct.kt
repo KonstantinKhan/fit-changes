0bcef21848bd99c08a8975e6825151b85dfd09d2
KonstantinKhan <khan366kos@gmail.com>
2022-04-11 23:02:26 +0500

    Created BaseInitProduct.kt

---
+++ fit_changes-be-repo-test/src/main/kotlin/ru/fit_changes/backend/repo/test/BaseInitProduct.kt
@@ -0,0 +1,19 @@
+package ru.fit_changes.backend.repo.test
+
+import ru.fit_changes.backend.common.product.models.*
+import java.util.*
+
+abstract class BaseInitProduct : IInitObjects<ProductModel> {
+    fun createInitTestModel(
+        suf: String,
+        productName: String,
+    ) = ProductModel(
+        productId = ProductIdModel(UUID.randomUUID()),
+        authorId = AuthorIdModel(UUID.randomUUID()),
+        productName = "$suf $productName",
+        caloriesPerHundredGrams = CaloriesModel(calories = 110.0),
+        proteinsPerHundredGrams = ProteinsModel(proteins = 21.0),
+        fatsPerHundredGrams = FatsModel(fats = 3.0),
+        carbohydratesPerHundredGrams = CarbohydratesModel(carbohydrates = 0.0),
+    )
+}
