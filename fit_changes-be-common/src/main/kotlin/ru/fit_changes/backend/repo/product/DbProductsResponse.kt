be500f0cbfe024e0014cb1bcba51ce8fb8397939
KonstantinKhan <khan366kos@gmail.com>
2022-04-21 22:49:21 +0500

    Added IDbResponse.

--- fit_changes-be-common/src/main/kotlin/ru/fit_changes/backend/repo/product/DbProductsResponse.kt
+++ fit_changes-be-common/src/main/kotlin/ru/fit_changes/backend/repo/product/DbProductsResponse.kt
@@ -1,4 +1,11 @@
 package ru.fit_changes.backend.repo.product
 
-class DbProductsResponse {
-}
+import ru.fit_changes.backend.common.models.CommonErrorModel
+import ru.fit_changes.backend.common.product.models.ProductModel
+import ru.fit_changes.backend.repo.IDbResponse
+
+class DbProductsResponse(
+    override val isSuccess: Boolean,
+    override val errors: List<CommonErrorModel> = emptyList(),
+    override val result: List<ProductModel>?
+) : IDbResponse<List<ProductModel>>
