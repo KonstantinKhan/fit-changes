d9b3f071afed48d9a7aa989a8cb850a119cb4b5d
KonstantinKhan <khan366kos@gmail.com>
2022-04-22 17:58:25 +0500

    Updated ktor.

--- fit_changes-be-product-app-ktor/src/main/kotlin/ru/fit_changes/backend/app/ktor/product/Application.kt
+++ fit_changes-be-product-app-ktor/src/main/kotlin/ru/fit_changes/backend/app/ktor/product/Application.kt
@@ -2,17 +2,19 @@ package ru.fit_changes.backend.app.ktor.product
 
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.SerializationFeature
-import io.ktor.application.*
-import io.ktor.features.*
+import io.ktor.server.application.*
 import io.ktor.http.*
-import io.ktor.jackson.*
-import io.ktor.response.*
-import io.ktor.routing.*
+import io.ktor.serialization.jackson.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.plugins.cors.CORS
 import ru.fit_changes.backend.product.service.ProductService
 import ru.fit_changes.backend.app.ktor.product.configs.AppKtorConfig
 import ru.fit_changes.backend.app.ktor.product.routes.registerProductRoutesHttp
 import ru.fit_changes.backend.product.logics.ProductCrud
+import ru.fit_changes.backend.repo.product.IRepoProduct
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
@@ -22,10 +24,13 @@ fun Application.module(
 ) {
     val productService = ProductService(ProductCrud(config.contextConfig))
 
+    println("repoProductTest is NONE: ${config.contextConfig.repoProductTest == IRepoProduct.NONE}")
+    println("repoProductProd is NONE: ${config.contextConfig.repoProductProd == IRepoProduct.NONE}")
+
     install(CORS) {
-        host("localhost:4200")
-        header(HttpHeaders.AccessControlAllowOrigin)
-        header(HttpHeaders.ContentType)
+        allowHost("localhost:4200")
+        allowHeader(HttpHeaders.AccessControlAllowOrigin)
+        allowHeader(HttpHeaders.ContentType)
     }
 
     install(ContentNegotiation) {
@@ -38,7 +43,6 @@ fun Application.module(
 
     routing {
         get("/") {
-            println(call.response.headers.allValues())
             call.respondText("Hello, World")
         }
     }
--- test_kafka/src/main/kotlin/ru/fit_changes/kafka_test/Application.kt
+++ test_kafka/src/main/kotlin/ru/fit_changes/kafka_test/Application.kt
@@ -1,9 +1,9 @@
 package ru.fit_changes.kafka_test
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.routing.*
+import io.ktor.server.application.*
 import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 import ru.fit_changes.kafka_test.plugins.kafka
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
