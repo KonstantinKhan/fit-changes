40f5964b2cee5bab23bb9b474ce5d6cd5ea2ada7
KonstantinKhan <khan366kos@gmail.com>
2022-04-22 20:20:18 +0500

    Updated create fun.

--- fit_changes-be-cassandra/src/main/kotlin/ru/fit_changes/backend/repo/cassandra/RepoProductCassandra.kt
+++ fit_changes-be-cassandra/src/main/kotlin/ru/fit_changes/backend/repo/cassandra/RepoProductCassandra.kt
@@ -2,7 +2,7 @@ package ru.fit_changes.backend.repo.cassandra
 
 import kotlinx.coroutines.future.await
 import ru.fit_changes.backend.common.models.CommonErrorModel
-import ru.fit_changes.backend.common.product.models.ProductIdModel
+import ru.fit_changes.backend.common.product.models.*
 import ru.fit_changes.backend.repo.product.*
 import java.util.*
 
@@ -11,6 +11,56 @@ class RepoProductCassandra(
 ) : IRepoProduct {
     override suspend fun create(req: DbProductModelRequest): DbProductResponse {
         val product = req.product.copy(productId = ProductIdModel(UUID.randomUUID()))
+        val errors = mutableListOf<CommonErrorModel>()
+        with(product) {
+            if (productName.isBlank()) {
+                errors.add(
+                    CommonErrorModel(
+                        field = "productName",
+                        message = "Product name cannot be null or empty"
+                    )
+                )
+            }
+            if (caloriesPerHundredGrams == CaloriesModel.NONE) {
+                errors.add(
+                    CommonErrorModel(
+                        field = "caloriesPerHundredGrams",
+                        message = "Calories cannot be null or empty"
+                    )
+                )
+            }
+            if (proteinsPerHundredGrams == ProteinsModel.NONE) {
+                errors.add(
+                    CommonErrorModel(
+                        field = "proteinsPerHundredGrams",
+                        message = "Proteins cannot be null or empty"
+                    )
+                )
+            }
+            if (fatsPerHundredGrams == FatsModel.NONE) {
+                errors.add(
+                    CommonErrorModel(
+                        field = "fatsPerHundredGrams",
+                        message = "Fats cannot be null or empty"
+                    )
+                )
+            }
+            if (carbohydratesPerHundredGrams == CarbohydratesModel.NONE) {
+                errors.add(
+                    CommonErrorModel(
+                        field = "carbohydratesPerHundredGrams",
+                        message = "Carbohydrates cannot be null or empty"
+                    )
+                )
+            }
+        }
+        if (errors.size > 0) {
+            return DbProductResponse(
+                result = null,
+                isSuccess = false,
+                errors = errors
+            )
+        }
         dao.create(ProductCassandraDTO(product)).await()
         val created = dao.read(product.productId.asString()).await()
         return if (created == null) {
