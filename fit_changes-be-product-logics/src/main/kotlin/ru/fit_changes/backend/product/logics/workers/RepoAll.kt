991e100b6044430599130431dce927f7a3b660e0
KonstantinKhan <khan366kos@gmail.com>
2022-04-22 17:47:33 +0500

    Created function for getting all products.

---
+++ fit_changes-be-product-logics/src/main/kotlin/ru/fit_changes/backend/product/logics/workers/RepoAll.kt
@@ -0,0 +1,22 @@
+package ru.fit_changes.backend.product.logics.workers
+
+import ru.fit_changes.backend.common.context.BeContext
+import ru.fit_changes.backend.common.context.CorStatus
+import ru.fit_changes.backend.product.logics.handlers.CorChainDsl
+import ru.fit_changes.backend.product.logics.handlers.addCorWorkerDsl
+
+fun CorChainDsl<BeContext>.repoAll(title: String) = addCorWorkerDsl {
+    this.title = title
+    on {
+        status == CorStatus.RUNNING
+    }
+    handle {
+        val result = productRepo.allProducts()
+        val resultValues = result.result
+        if (result.isSuccess && !resultValues.isNullOrEmpty()) {
+            foundProducts = resultValues.toMutableList()
+        } else {
+            result.errors.forEach { addError(it) }
+        }
+    }
+}
