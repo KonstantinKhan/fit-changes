ef2bfd4152903e97b6d998062869593491ad21b0
KonstantinKhan <khan366kos@gmail.com>
2022-04-26 10:19:11 +0500

    Updated ration page component for show products information.

--- fit_changes-client-angular/src/app/ration/ration-page/ration-page.component.ts
+++ fit_changes-client-angular/src/app/ration/ration-page/ration-page.component.ts
@@ -3,6 +3,7 @@ import {DynamicModalLoader} from "../../shared/directives/load-modal.directive";
 import {ModalCreateProductComponent} from "../../shared/components/modal-create-product/modal-create-product.component";
 import {Subscription} from "rxjs";
 import {HttpClient} from "@angular/common/http";
+import {Product} from "../../shared/interfaces/product";
 
 @Component({
   selector: 'app-ration-page',
@@ -11,7 +12,7 @@ import {HttpClient} from "@angular/common/http";
 })
 export class RationPageComponent implements OnInit {
 
-  products: string[] = [];
+  products: Product[] = [];
 
   @ViewChild(DynamicModalLoader) dynamicModal!: DynamicModalLoader
 
@@ -22,16 +23,54 @@ export class RationPageComponent implements OnInit {
 
   ngOnInit(): void {
     // this.showModal()
+    this.http.post('http://localhost:8080/product/search', {
+        messageType: "SearchProductRequest",
+        requestId: "rID:0006",
+        query: "",
+        debug: {
+          mode: "test"
+        },
+      },
+      {
+        responseType: "json"
+      }).subscribe(products => {
+      Object.entries(products).find(([key, value]) => {
+        if (key === 'foundProducts') {
+          this.products = value as Product[]
+          this.products.forEach(product => {
+            console.log(product.productName)
+          })
+        }
+      })
+    })
   }
 
   showModal() {
     const component = this.dynamicModal.viewContainerRef.createComponent(ModalCreateProductComponent)
     component.instance.close.subscribe(() => {
-      console.log(component.instance.product.productName)
-      if (component.instance.product.productName != null) {
-        this.products.push(component.instance.product.productName)
-      }
-      console.log(this.products)
+
+      this.http.post('http://localhost:8080/product/search', {
+          messageType: "SearchProductRequest",
+          requestId: "rID:0006",
+          query: "",
+          debug: {
+            mode: "test"
+          },
+        },
+        {
+          responseType: "json"
+        }).subscribe(products => {
+        Object.entries(products).find(([key, value]) => {
+          if (key === 'foundProducts') {
+            this.products = value as Product[]
+            this.products.forEach(product => {
+              console.log(product.productName)
+            })
+          }
+        })
+      })
+
+
       this.dynamicModal.viewContainerRef.clear()
       // component.instance.close.unsubscribe()
     })
@@ -42,8 +81,7 @@ export class RationPageComponent implements OnInit {
       console.log(text)
     })
     this.http.post('http://localhost:8080/product/create',
-      {
-      },
+      {},
     ).subscribe(response => console.log(response))
   }
 }
