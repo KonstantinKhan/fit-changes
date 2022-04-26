4f0c7ba93997df4096e3fe49d9f5d2c347a19ea7
KonstantinKhan <khan366kos@gmail.com>
2022-04-26 10:22:39 +0500

    Updated tests in RepoProductSearchTest.kt

--- fit_changes-be-repo-test/src/main/kotlin/ru/fit_changes/backend/repo/test/RepoProductSearchTest.kt
+++ fit_changes-be-repo-test/src/main/kotlin/ru/fit_changes/backend/repo/test/RepoProductSearchTest.kt
@@ -6,17 +6,33 @@ import ru.fit_changes.backend.repo.product.DbProductFilterRequest
 import ru.fit_changes.backend.repo.product.IRepoProduct
 import ru.fit_changes.backend.utils.product.BEEF_FILLED_MODEL
 import ru.fit_changes.backend.utils.product.CHICKEN_FILLED_MODEL
+import ru.fit_changes.backend.utils.product.CHICKEN_THIGH_FILLED_MODEL
 import java.util.UUID
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 abstract class RepoProductSearchTest {
     abstract val repo: IRepoProduct
 
     @Test
-    fun searchSuccess() {
+    fun searchSuccessWithoutQuery() {
         val result = runBlocking { repo.search(DbProductFilterRequest()) }
         assertTrue(result.isSuccess)
+    }
+
+    @Test
+    fun searchSuccessBeef() {
+        val result = runBlocking { repo.search(DbProductFilterRequest("Гов")) }
+        assertTrue(result.isSuccess)
+        assertTrue(result.result?.size == 1)
+        assertEquals(initObjects.first(), result.result?.first())
+    }
+
+    @Test
+    fun searchSuccessChicken() {
+        val result = runBlocking { repo.search(DbProductFilterRequest("кур")) }
+        assertEquals(2, result.result?.size)
     }
 
     companion object : BaseInitProduct() {
@@ -25,6 +41,9 @@ abstract class RepoProductSearchTest {
                 productId = ProductIdModel(UUID.randomUUID())
             ),
             CHICKEN_FILLED_MODEL.copy(
+                productId = ProductIdModel(UUID.randomUUID())
+            ),
+            CHICKEN_THIGH_FILLED_MODEL.copy(
                 productId = ProductIdModel(UUID.randomUUID())
             )
         )
