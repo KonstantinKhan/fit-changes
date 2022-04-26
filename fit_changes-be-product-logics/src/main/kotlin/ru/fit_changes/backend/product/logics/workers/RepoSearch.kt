cbc8096958b9720cdbcf46b4defcb0a335804a51
KonstantinKhan <khan366kos@gmail.com>
2022-04-26 10:17:08 +0500

    Created RepoSearch.kt

---
+++ fit_changes-be-product-logics/src/main/kotlin/ru/fit_changes/backend/product/logics/workers/RepoSearch.kt
@@ -0,0 +1,25 @@
+package ru.fit_changes.backend.product.logics.workers
+
+import ru.fit_changes.backend.common.context.BeContext
+import ru.fit_changes.backend.common.context.CorStatus
+import ru.fit_changes.backend.common.product.models.ProductModel
+import ru.fit_changes.backend.product.logics.handlers.CorChainDsl
+import ru.fit_changes.backend.product.logics.handlers.addCorWorkerDsl
+import ru.fit_changes.backend.repo.product.DbProductFilterRequest
+
+fun CorChainDsl<BeContext>.repoSearch(title: String) = addCorWorkerDsl {
+    this.title = title
+    on {
+        status == CorStatus.RUNNING
+    }
+    handle {
+        val result = productRepo.search(DbProductFilterRequest(requestProductFilter.searchStr))
+        println("result: ${result.result}")
+        val resultValue = result.result
+        if (result.isSuccess && resultValue != null) {
+            foundProducts = resultValue as MutableList<ProductModel>
+        } else {
+            result.errors.forEach { addError(it) }
+        }
+    }
+}
