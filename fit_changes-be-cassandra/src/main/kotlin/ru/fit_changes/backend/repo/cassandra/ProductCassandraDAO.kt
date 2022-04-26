903f2dedff5d0c83d3d97b3beb133a9476b3a784
KonstantinKhan <khan366kos@gmail.com>
2022-04-26 10:16:29 +0500

    Implemented search in ProductCassandraDAO.kt

--- fit_changes-be-cassandra/src/main/kotlin/ru/fit_changes/backend/repo/cassandra/ProductCassandraDAO.kt
+++ fit_changes-be-cassandra/src/main/kotlin/ru/fit_changes/backend/repo/cassandra/ProductCassandraDAO.kt
@@ -2,7 +2,9 @@ package ru.fit_changes.backend.repo.cassandra
 
 import com.datastax.oss.driver.api.mapper.annotations.Dao
 import com.datastax.oss.driver.api.mapper.annotations.Insert
+import com.datastax.oss.driver.api.mapper.annotations.QueryProvider
 import com.datastax.oss.driver.api.mapper.annotations.Select
+import ru.fit_changes.backend.repo.product.DbProductFilterRequest
 import java.util.concurrent.CompletionStage
 
 @Dao
@@ -12,4 +14,10 @@ interface ProductCassandraDAO {
 
     @Select
     fun read(id: String): CompletionStage<ProductCassandraDTO?>
+
+    @QueryProvider(
+        providerClass = ProductCassandraSearchProvider::class,
+        entityHelpers = [ProductCassandraDTO::class]
+    )
+    fun search(req: DbProductFilterRequest): CompletionStage<Collection<ProductCassandraDTO>>
 }
