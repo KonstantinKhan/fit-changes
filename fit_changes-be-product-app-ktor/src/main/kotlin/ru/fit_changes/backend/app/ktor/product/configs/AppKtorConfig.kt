2b090fbcdf90a457f15444b052f1123daa79d653
KonstantinKhan <khan366kos@gmail.com>
2022-04-26 10:11:59 +0500

    Added primary content for repo im memory.

--- fit_changes-be-product-app-ktor/src/main/kotlin/ru/fit_changes/backend/app/ktor/product/configs/AppKtorConfig.kt
+++ fit_changes-be-product-app-ktor/src/main/kotlin/ru/fit_changes/backend/app/ktor/product/configs/AppKtorConfig.kt
@@ -1,13 +1,24 @@
 package ru.fit_changes.backend.app.ktor.product.configs
 
 import ru.fit_changes.backend.common.context.ContextConfig
+import ru.fit_changes.backend.common.product.models.ProductIdModel
 import ru.fit_changes.backend.repo.cassandra.CassandraObject
 import ru.fit_changes.backend.repo.inmemory.RepoProductInMemory
 import ru.fit_changes.backend.repo.product.IRepoProduct
+import ru.fit_changes.backend.utils.product.BEEF_FILLED_MODEL
+import ru.fit_changes.backend.utils.product.CHICKEN_FILLED_MODEL
+import ru.fit_changes.backend.utils.product.CHICKEN_THIGH_FILLED_MODEL
+import java.util.*
 
 data class AppKtorConfig(
     val contextConfig: ContextConfig = ContextConfig(
-        repoProductTest = RepoProductInMemory(),
+        repoProductTest = RepoProductInMemory(
+            listOf(
+                BEEF_FILLED_MODEL.copy(productId = ProductIdModel(UUID.randomUUID())),
+                CHICKEN_FILLED_MODEL.copy(productId = ProductIdModel(UUID.randomUUID())),
+                CHICKEN_THIGH_FILLED_MODEL.copy(productId = ProductIdModel(UUID.randomUUID()))
+            )
+        ),
         repoProductProd = try {
             CassandraObject.createRepo()
         } catch (e: Exception) {
