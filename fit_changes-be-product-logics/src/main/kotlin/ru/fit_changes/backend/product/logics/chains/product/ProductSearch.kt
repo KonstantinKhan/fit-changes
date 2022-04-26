1de828e5748ac56db8d7430e31d1fbf669e1700e
KonstantinKhan <khan366kos@gmail.com>
2022-04-26 10:17:48 +0500

    Added repoSearch worker.

--- fit_changes-be-product-logics/src/main/kotlin/ru/fit_changes/backend/product/logics/chains/product/ProductSearch.kt
+++ fit_changes-be-product-logics/src/main/kotlin/ru/fit_changes/backend/product/logics/chains/product/ProductSearch.kt
@@ -6,9 +6,11 @@ import ru.fit_changes.backend.product.logics.chain
 import ru.fit_changes.backend.product.logics.chains.stub.productSearchStub
 import ru.fit_changes.backend.product.logics.workers.chainInitWorker
 import ru.fit_changes.backend.product.logics.workers.chooseDb
+import ru.fit_changes.backend.product.logics.workers.repoSearch
 
 object ProductSearch : ICorExecutor<BeContext> by chain<BeContext>({
     chainInitWorker("Initialization of chain")
     chooseDb("Choose DB or STUB")
     productSearchStub("Processing of stub case SEARCH")
+    repoSearch("Search in DB")
 }).build()
