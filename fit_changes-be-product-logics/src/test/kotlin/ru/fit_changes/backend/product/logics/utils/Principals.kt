7964c4662a8100ac2f949f7aa481a85d06eb0110
KonstantinKhan <khan366kos@gmail.com>
2022-06-04 08:44:44 +0500

    Renamed property.

--- fit_changes-be-product-logics/src/test/kotlin/ru/fit_changes/backend/product/logics/utils/Principals.kt
+++ fit_changes-be-product-logics/src/test/kotlin/ru/fit_changes/backend/product/logics/utils/Principals.kt
@@ -6,7 +6,7 @@ import ru.fit_changes.backend.common.product.models.AuthorIdModel
 import ru.fit_changes.backend.utils.product.AUTHOR_ID_0001
 
 fun testUser(): BePrincipalModel = BePrincipalModel(
-    id = AuthorIdModel(id = AUTHOR_ID_0001),
+    authorId = AuthorIdModel(id = AUTHOR_ID_0001),
     firstName = "Konstantin",
     lastName = "Khan",
     groups = setOf(
@@ -16,7 +16,7 @@ fun testUser(): BePrincipalModel = BePrincipalModel(
 )
 
 fun testUserBan(): BePrincipalModel = BePrincipalModel(
-    id = AuthorIdModel(id = "aID:0001"),
+    authorId = AuthorIdModel(id = "aID:0001"),
     firstName = "Konstantin",
     lastName = "Khan",
     groups = setOf(
