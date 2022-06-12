873a484d9041cfdac95bde3c2f6a21dd6e7d7588
KonstantinKhan <khan366kos@gmail.com>
2022-06-13 00:21:22 +0500

    Created module.

---
+++ fit_changes-be-mapping-ration/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "ru.fitChanges"
+version = "0.0.1"
+
+dependencies {
+    implementation(kotlin("stdlib"))
+
+    implementation(project(":fit_changes-be-common"))
+    implementation(project(":fit_changes-be-transport-openapi-ration"))
+    testImplementation(project(":fit_changes-be-utils"))
+
+    testImplementation(kotlin("test"))
+}
