1ed8c15acc85d901999e35e6d1c8be96a59665f2
KonstantinKhan <khan366kos@gmail.com>
2022-06-13 22:00:23 +0500

    Updated BeMeal.kt

--- fit_changes-be-common/src/main/kotlin/ru/fit_changes/backend/common/models/ration/BeMeal.kt
+++ fit_changes-be-common/src/main/kotlin/ru/fit_changes/backend/common/models/ration/BeMeal.kt
@@ -1,6 +1,8 @@
 package ru.fit_changes.backend.common.models.ration
 
+import ru.fit_changes.backend.common.models.enums.BeMealName
+
 data class BeMeal(
-    val mealName: String,
+    val mealName: BeMealName,
     val usedProducts: MutableList<BeUsedProduct> = mutableListOf()
 )
