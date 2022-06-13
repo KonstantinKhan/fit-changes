d8196f82bad396284cab8411039f9bcfd8be080c
KonstantinKhan <khan366kos@gmail.com>
2022-06-13 22:01:43 +0500

    Moved mappers

--- fit_changes-be-common/build.gradle.kts
+++ fit_changes-be-common/build.gradle.kts
@@ -7,4 +7,6 @@ version = "0.0.1"
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation(project(":fit_changes-be-transport-openapi-product"))
+    implementation(project(":fit_changes-be-transport-openapi-ration"))
 }
