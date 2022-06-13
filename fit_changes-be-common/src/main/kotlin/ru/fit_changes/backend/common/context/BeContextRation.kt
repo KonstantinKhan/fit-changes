f0eeef4027ef2e2667c7af5a996309655434f2f3
KonstantinKhan <khan366kos@gmail.com>
2022-06-13 22:00:10 +0500

    Updated BeContextRation.kt

--- fit_changes-be-common/src/main/kotlin/ru/fit_changes/backend/common/context/BeContextRation.kt
+++ fit_changes-be-common/src/main/kotlin/ru/fit_changes/backend/common/context/BeContextRation.kt
@@ -1,10 +1,19 @@
 package ru.fit_changes.backend.common.context
 
+import ru.fit_changes.backend.common.models.IError
+import ru.fit_changes.backend.common.models.ration.RationIdModel
 import ru.fit_changes.backend.common.models.ration.RationModel
+import ru.fit_changes.backend.common.models.ration.RationSearchFilter
 
 data class BeContextRation(
     var requestId: String = "",
 
-    var requestRation: RationModel = RationModel()
+    var requestRation: RationModel = RationModel(),
+    var requestRationId: RationIdModel = RationIdModel.NONE,
+    var responseRation: RationModel = RationModel(),
+
+    var requestRationFilter: RationSearchFilter = RationSearchFilter(),
+
+    val errors: MutableList<IError> = mutableListOf()
 )
 
