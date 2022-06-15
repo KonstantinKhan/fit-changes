749dd04b2de4678bfdecc72705dcbcde7b47fc07
KonstantinKhan <khan366kos@gmail.com>
2022-06-15 21:32:18 +0500

    Added mappers in Context2TransportRation.kt

--- fit_changes-be-mapping-ration/src/main/kotlin/ru/fit_changes/backend/mapping/ration/Context2TransportRation.kt
+++ fit_changes-be-mapping-ration/src/main/kotlin/ru/fit_changes/backend/mapping/ration/Context2TransportRation.kt
@@ -4,7 +4,7 @@ import ru.fit_changes.backend.common.context.BeContextRation
 import ru.fit_changes.backend.common.mappers.toTransport
 import ru.fit_changes.backend.common.models.IError
 import ru.fit_changes.backend.common.models.ration.RationModel
-import ru.fit_changes.openapi.models.CreateRationResponse
+import ru.fit_changes.openapi.models.*
 
 fun BeContextRation.toCreateRationResponse() = CreateRationResponse(
     messageType = "CreateRationResponse",
@@ -13,4 +13,40 @@ fun BeContextRation.toCreateRationResponse() = CreateRationResponse(
     else CreateRationResponse.Result.ERROR,
     errors = errors.takeIf { it.isNotEmpty() }?.map { it.toTransport() },
     createdRation = responseRation.takeIf { errors.isEmpty() && it != RationModel() }?.toTransport()
+)
+
+fun BeContextRation.toReadRationResponse() = ReadRationResponse(
+    messageType = "ReadRationResponse",
+    requestId = requestId,
+    result = if (errors.find { it.level == IError.Level.ERROR } == null) ReadRationResponse.Result.SUCCESS
+    else ReadRationResponse.Result.ERROR,
+    errors = errors.takeIf { it.isNotEmpty() }?.map { it.toTransport() },
+    readRation = responseRation.takeIf { errors.isEmpty() && it != RationModel() }?.toTransport()
+)
+
+fun BeContextRation.toUpdateRationResponse() = UpdateRationResponse(
+    messageType = "UpdateRationResponse",
+    requestId = requestId,
+    result = if (errors.find { it.level == IError.Level.ERROR } == null) UpdateRationResponse.Result.SUCCESS
+    else UpdateRationResponse.Result.ERROR,
+    errors = errors.takeIf { it.isNotEmpty() }?.map { it.toTransport() },
+    updatedRation = responseRation.takeIf { errors.isEmpty() && it != RationModel() }?.toTransport()
+)
+
+fun BeContextRation.toDeleteRationResponse() = DeleteRationResponse(
+    messageType = "DeleteRationResponse",
+    requestId = requestId,
+    result = if (errors.find { it.level == IError.Level.ERROR } == null) DeleteRationResponse.Result.SUCCESS
+    else DeleteRationResponse.Result.ERROR,
+    errors = errors.takeIf { it.isNotEmpty() }?.map { it.toTransport() },
+    deletedRation = responseRation.takeIf { errors.isEmpty() && it != RationModel() }?.toTransport()
+)
+
+fun BeContextRation.toSearchRationResponse() = SearchRationResponse(
+    messageType = "SearchRationResponse",
+    requestId = requestId,
+    result = if (errors.find { it.level == IError.Level.ERROR } == null) SearchRationResponse.Result.SUCCESS
+    else SearchRationResponse.Result.ERROR,
+    errors = errors.takeIf { it.isNotEmpty() }?.map { it.toTransport() },
+    foundRations = foundRations.map { it.toTransport() }
 )
