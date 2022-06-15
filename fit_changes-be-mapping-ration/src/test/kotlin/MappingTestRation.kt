d1589622c11bfc0536f0cc8af48237bc80611ef3
KonstantinKhan <khan366kos@gmail.com>
2022-06-15 21:32:45 +0500

    Added tests in MappingTestRation.kt

--- fit_changes-be-mapping-ration/src/test/kotlin/MappingTestRation.kt
+++ fit_changes-be-mapping-ration/src/test/kotlin/MappingTestRation.kt
@@ -7,8 +7,7 @@ import ru.fit_changes.backend.common.models.FatsModel
 import ru.fit_changes.backend.common.models.ProteinsModel
 import ru.fit_changes.backend.common.models.ration.RationIdModel
 import ru.fit_changes.backend.common.models.ration.RationSearchFilter
-import ru.fit_changes.backend.mapping.ration.setQuery
-import ru.fit_changes.backend.mapping.ration.toCreateRationResponse
+import ru.fit_changes.backend.mapping.ration.*
 import ru.fit_changes.backend.utils.product.*
 import ru.fit_changes.openapi.models.*
 import java.time.Instant
@@ -139,5 +138,112 @@ class MappingTestRation {
             assertTrue(errors.isNullOrEmpty())
             assertFalse(createdRation?.meals.isNullOrEmpty())
         }
+        with(response.createdRation) {
+            assertEquals(2000.0, this?.caloriesNorm)
+            assertEquals(100.0, this?.proteinsNorm)
+            assertEquals(50.0, this?.fatsNorm)
+            assertEquals(200.0, this?.carbohydratesNorm)
+            assertEquals(2200.0, this?.caloriesFact)
+            assertEquals(120.0, this?.proteinsFact)
+            assertEquals(56.0, this?.fatsFact)
+            assertEquals(240.0, this?.carbohydratesFact)
+        }
+        with(response.createdRation?.meals?.first()?.products?.first()) {
+            assertEquals(AUTHOR_ID_0001, this?.authorId)
+            assertEquals("Филе куриной грудки", this?.productName)
+            assertEquals(110.0, this?.caloriesPerHundredGrams)
+            assertEquals(21.0, this?.proteinsPerHundredGrams)
+            assertEquals(3.0, this?.fatsPerHundredGrams)
+            assertEquals(0.0, this?.carbohydratesPerHundredGrams)
+            assertEquals(200.0, this?.weight)
+            assertEquals(220.0, this?.caloriesFact)
+            assertEquals(42.0, this?.proteinsFact)
+            assertEquals(6.0, this?.fatsFact)
+            assertEquals(0.0, this?.carbohydratesFact)
+            assertEquals(PRODUCT_ID_0002, this?.parentProductId)
+        }
+    }
+
+    @Test
+    fun readRationResponseSuccess() {
+        val beContextRation = BeContextRation().apply {
+            requestId = REQUEST_ID_0001
+            responseRation = RATION_FILLED_MODEL.apply {
+                rationId = RationIdModel(RATION_ID)
+            }
+        }
+
+        val response = beContextRation.toReadRationResponse()
+        with(response) {
+            assertEquals(this::class.simpleName, response.messageType)
+            assertEquals(REQUEST_ID_0001, requestId)
+            assertEquals(ReadRationResponse.Result.SUCCESS, result)
+            assertTrue(errors.isNullOrEmpty())
+            assertFalse(readRation?.meals.isNullOrEmpty())
+        }
+        with(response.readRation) {
+            assertEquals(2000.0, this?.caloriesNorm)
+            assertEquals(100.0, this?.proteinsNorm)
+            assertEquals(50.0, this?.fatsNorm)
+            assertEquals(200.0, this?.carbohydratesNorm)
+            assertEquals(2200.0, this?.caloriesFact)
+            assertEquals(120.0, this?.proteinsFact)
+            assertEquals(56.0, this?.fatsFact)
+            assertEquals(240.0, this?.carbohydratesFact)
+        }
+    }
+
+    @Test
+    fun updateRationResponseSuccess() {
+        val beContextRation = BeContextRation().apply {
+            requestId = REQUEST_ID_0001
+            responseRation = RATION_FILLED_MODEL.apply {
+                rationId = RationIdModel(RATION_ID)
+            }
+        }
+        val response = beContextRation.toUpdateRationResponse()
+        with(response) {
+            assertEquals(this::class.simpleName, response.messageType)
+            assertEquals(REQUEST_ID_0001, requestId)
+            assertEquals(UpdateRationResponse.Result.SUCCESS, result)
+            assertTrue(errors.isNullOrEmpty())
+            assertFalse(updatedRation?.meals.isNullOrEmpty())
+        }
+    }
+
+    @Test
+    fun deleteRationResponseSuccess() {
+        val beContextRation = BeContextRation().apply {
+            requestId = REQUEST_ID_0001
+            responseRation = RATION_FILLED_MODEL.apply {
+                rationId = RationIdModel(RATION_ID)
+            }
+        }
+
+        val response = beContextRation.toDeleteRationResponse()
+        with(response) {
+            assertEquals(this::class.simpleName, response.messageType)
+            assertEquals(REQUEST_ID_0001, requestId)
+            assertEquals(DeleteRationResponse.Result.SUCCESS, result)
+            assertTrue(errors.isNullOrEmpty())
+            assertFalse(deletedRation?.meals.isNullOrEmpty())
+        }
+    }
+
+    @Test
+    fun searchRationResponseSuccess() {
+        val beContextRation = BeContextRation().apply {
+            requestId = REQUEST_ID_0001
+            foundRations = mutableListOf(RATION_FILLED_MODEL)
+        }
+        val response = beContextRation.toSearchRationResponse()
+        with(response) {
+            assertEquals(this::class.simpleName, response.messageType)
+            assertEquals(REQUEST_ID_0001, requestId)
+            assertEquals(SearchRationResponse.Result.SUCCESS, result)
+            assertTrue(errors.isNullOrEmpty())
+            assertFalse(this.foundRations.isNullOrEmpty())
+        }
+
     }
 }
