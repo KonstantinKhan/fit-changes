60512cc631c677190c77f3b6e3983e6972704d4c
KonstantinKhan <khan366kos@gmail.com>
2022-06-16 23:08:59 +0500

    Created service module.

---
+++ fit_changes-be-ration-service/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    kotlin("jvm")
+}
+
+dependencies {
+
+    val coroutinesVersion: String by project
+
+    implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+
+    implementation(project(":fit_changes-be-transport-openapi-ration"))
+    implementation(project(":fit_changes-be-common"))
+    implementation(project(":fit_changes-be-mapping-ration"))
+
+}
