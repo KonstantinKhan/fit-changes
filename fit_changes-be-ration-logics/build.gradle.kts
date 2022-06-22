2f03532590c5fa3815c1970501e0ba64c4cd92c4
KonstantinKhan <khan366kos@gmail.com>
2022-06-22 20:23:31 +0500

    Created ration logics module.

---
+++ fit_changes-be-ration-logics/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    kotlin("jvm")
+}
+
+dependencies {
+    val coroutinesVersion: String by project
+
+    implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+
+    implementation(project(":fit_changes-be-common"))
+    implementation(project(":fit_changes-be-utils"))
+    implementation(project(":fit_changes-cor"))
+
+    testImplementation(kotlin("test"))
+}
