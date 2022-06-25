8cd242d0a0c3e5dc66ff1c4eb3fa69422157b42a
KonstantinKhan <khan366kos@gmail.com>
2022-06-25 13:15:17 +0500

    Fixed bug.

--- fit_changes-be-ration-logics/src/main/kotlin/ru/fit_changes/backend/ration/logics/stubs/RationCreateStub.kt
+++ fit_changes-be-ration-logics/src/main/kotlin/ru/fit_changes/backend/ration/logics/stubs/RationCreateStub.kt
@@ -4,7 +4,6 @@ import ru.fit_changes.backend.common.context.BeContextRation
 import ru.fit_changes.backend.common.context.CorStatus
 import ru.fit_changes.backend.common.models.enums.StubCases
 import ru.fit_changes.backend.common.models.ration.RationIdModel
-import ru.fit_changes.backend.utils.product.RATION_FILLED_MODEL
 import ru.fit_changes.backend.utils.product.RATION_ID
 import ru.fit_changes.cor.CorChainDsl
 import ru.fit_changes.cor.addCorWorkerDsl
@@ -22,7 +21,7 @@ fun CorChainDsl<BeContextRation>.rationCreateStub(title: String) = chain {
             stubCase == StubCases.SUCCESS
         }
         handle {
-            responseRation = RATION_FILLED_MODEL.copy(
+            responseRation = requestRation.copy(
                 rationId = RationIdModel(RATION_ID)
             )
         }
