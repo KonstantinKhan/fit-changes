5b86e1a40203a00282d85067ce60a3b0be6c83b3
KonstantinKhan <khan366kos@gmail.com>
2022-06-25 11:02:32 +0500

    Created ChainInitWorker.kt, CheckOperation.kt and RationCreateStub.kt

---
+++ fit_changes-be-ration-logics/src/main/kotlin/ru/fit_changes/backend/ration/logics/stubs/RationCreateStub.kt
@@ -0,0 +1,30 @@
+package ru.fit_changes.backend.ration.logics.stubs
+
+import ru.fit_changes.backend.common.context.BeContextRation
+import ru.fit_changes.backend.common.context.CorStatus
+import ru.fit_changes.backend.common.models.enums.StubCases
+import ru.fit_changes.backend.common.models.ration.RationIdModel
+import ru.fit_changes.backend.utils.product.RATION_FILLED_MODEL
+import ru.fit_changes.backend.utils.product.RATION_ID
+import ru.fit_changes.cor.CorChainDsl
+import ru.fit_changes.cor.addCorWorkerDsl
+import ru.fit_changes.cor.chain
+
+fun CorChainDsl<BeContextRation>.rationCreateStub(title: String) = chain {
+    this.title = title
+    on {
+        status == CorStatus.RUNNING &&
+                stubCase != StubCases.NONE
+    }
+    addCorWorkerDsl {
+        this.title = "Successful stub case CREATE"
+        on {
+            stubCase == StubCases.SUCCESS
+        }
+        handle {
+            responseRation = RATION_FILLED_MODEL.copy(
+                rationId = RationIdModel(RATION_ID)
+            )
+        }
+    }
+}
