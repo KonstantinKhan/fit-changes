0f53a5cdcc04e82f93e699dd43841d01fe0d2e6f
KonstantinKhan <khan366kos@gmail.com>
2022-06-25 14:42:53 +0500

    Created RationReadStub.kt

---
+++ fit_changes-be-ration-logics/src/main/kotlin/ru/fit_changes/backend/ration/logics/stubs/RationReadStub.kt
@@ -0,0 +1,24 @@
+package ru.fit_changes.backend.ration.logics.stubs
+
+import ru.fit_changes.backend.common.context.BeContextRation
+import ru.fit_changes.backend.common.context.CorStatus
+import ru.fit_changes.backend.common.models.enums.StubCases
+import ru.fit_changes.backend.common.models.ration.RationIdModel
+import ru.fit_changes.backend.utils.product.RATION_FILLED_MODEL
+import ru.fit_changes.backend.utils.product.RATION_ID
+import ru.fit_changes.cor.CorChainDsl
+import ru.fit_changes.cor.addCorWorkerDsl
+
+fun CorChainDsl<BeContextRation>.rationReadStub(title: String) = addCorWorkerDsl {
+    this.title = title
+    on {
+        stubCase == StubCases.SUCCESS &&
+                status == CorStatus.RUNNING
+    }
+
+    handle {
+        responseRation = RATION_FILLED_MODEL.copy(
+            rationId = RationIdModel(RATION_ID)
+        )
+    }
+}
