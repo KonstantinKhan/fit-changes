1480d235020b4f3821bb9f1c122c17ed5b828c72
KonstantinKhan <khan366kos@gmail.com>
2022-06-25 11:00:27 +0500

    Added and configuration content negotiation module.

--- fit_changes-be-ration-app-ktor/src/main/kotlin/ru/fit_changes/backend/app/ktor/ration/Application.kt
+++ fit_changes-be-ration-app-ktor/src/main/kotlin/ru/fit_changes/backend/app/ktor/ration/Application.kt
@@ -1,16 +1,27 @@
 package ru.fit_changes.backend.app.ktor.ration
 
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.SerializationFeature
+import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
 import io.ktor.server.netty.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
+import io.ktor.server.plugins.contentnegotiation.*
+import ru.fit_changes.backend.app.ktor.ration.routes.rationRoutes
+import ru.fit_changes.backend.ration.service.RationService
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 fun Application.module() {
-    routing {
-        get("/") {
-            call.respondText("Hello!!!")
+
+    install(ContentNegotiation) {
+        jackson {
+            disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+            enable(SerializationFeature.INDENT_OUTPUT)
+            writerWithDefaultPrettyPrinter()
         }
     }
+
+    val rationService = RationService()
+
+    rationRoutes(rationService)
 }
