2ed70153be6e149d1fc13ae40abcf7fd64f36e77
KonstantinKhan <khan366kos@gmail.com>
2022-06-25 15:23:18 +0500

    Removed unused imports.

--- fit_changes-be-ration-logics/src/main/kotlin/ru/fit_changes/backend/ration/logics/stubs/RationReadStub.kt
+++ fit_changes-be-ration-logics/src/main/kotlin/ru/fit_changes/backend/ration/logics/stubs/RationReadStub.kt
@@ -3,9 +3,7 @@ package ru.fit_changes.backend.ration.logics.stubs
 import ru.fit_changes.backend.common.context.BeContextRation
 import ru.fit_changes.backend.common.context.CorStatus
 import ru.fit_changes.backend.common.models.enums.StubCases
-import ru.fit_changes.backend.common.models.ration.RationIdModel
 import ru.fit_changes.backend.utils.product.RATION_FILLED_MODEL
-import ru.fit_changes.backend.utils.product.RATION_ID
 import ru.fit_changes.cor.CorChainDsl
 import ru.fit_changes.cor.addCorWorkerDsl
 
