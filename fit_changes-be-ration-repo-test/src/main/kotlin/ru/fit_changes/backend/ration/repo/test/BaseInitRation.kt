522387b27d764a5c463d9f4d1b41af955f83396c
KonstantinKhan <khan366kos@gmail.com>
2022-06-26 22:56:45 +0500

    Updated createInitTestModel().

--- fit_changes-be-ration-repo-test/src/main/kotlin/ru/fit_changes/backend/ration/repo/test/BaseInitRation.kt
+++ fit_changes-be-ration-repo-test/src/main/kotlin/ru/fit_changes/backend/ration/repo/test/BaseInitRation.kt
@@ -1,10 +1,14 @@
 package ru.fit_changes.backend.ration.repo.test
 
+import ru.fit_changes.backend.common.models.AuthorIdModel
+import ru.fit_changes.backend.common.models.ration.RationIdModel
 import ru.fit_changes.backend.common.models.ration.RationModel
 import ru.fit_changes.backend.utils.product.RATION_FILLED_MODEL
+import java.util.*
 
 abstract class BaseInitRation : IInitObjects<RationModel> {
-    fun createInitTestModel(
-        suf: String
-    ) = RATION_FILLED_MODEL
+    fun createInitTestModel() = RATION_FILLED_MODEL.copy(
+        rationId = RationIdModel(UUID.randomUUID()),
+        authorId = AuthorIdModel(UUID.randomUUID())
+    )
 }
