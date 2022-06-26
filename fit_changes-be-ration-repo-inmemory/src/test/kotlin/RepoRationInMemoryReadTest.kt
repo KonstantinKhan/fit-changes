a45d36355f000060471838bc3a48b06075078aaa
KonstantinKhan <khan366kos@gmail.com>
2022-06-26 22:59:43 +0500

    Updated RepoInMemoryReadTest.kt

--- fit_changes-be-ration-repo-inmemory/src/test/kotlin/RepoRationInMemoryReadTest.kt
+++ fit_changes-be-ration-repo-inmemory/src/test/kotlin/RepoRationInMemoryReadTest.kt
@@ -2,6 +2,6 @@ import ru.fit_changes.backend.ration.repo.inmemory.RepoRationInMemory
 import ru.fit_changes.backend.ration.repo.test.RepoRationReadTest
 import ru.fit_changes.backend.repo.ration.IRepoRation
 
-class RepoInMemoryReadTest : RepoRationReadTest() {
-    override val repo: IRepoRation = RepoRationInMemory()
+class RepoRationInMemoryReadTest: RepoRationReadTest() {
+    override val repo: IRepoRation = RepoRationInMemory(initObjects = initObjects)
 }
