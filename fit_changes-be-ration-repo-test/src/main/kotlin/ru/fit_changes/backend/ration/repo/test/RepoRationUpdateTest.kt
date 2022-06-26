2270ee8553aa21518221dab8d114f6147783005e
KonstantinKhan <khan366kos@gmail.com>
2022-06-26 23:22:15 +0500

    Created RepoRationInmemoryUpdateTest.kt and RepoRationUpdateTest.kt

---
+++ fit_changes-be-ration-repo-test/src/main/kotlin/ru/fit_changes/backend/ration/repo/test/RepoRationUpdateTest.kt
@@ -0,0 +1,33 @@
+package ru.fit_changes.backend.ration.repo.test
+
+import kotlinx.coroutines.runBlocking
+import ru.fit_changes.backend.common.models.AuthorIdModel
+import ru.fit_changes.backend.common.models.CaloriesModel
+import ru.fit_changes.backend.common.models.ration.RationIdModel
+import ru.fit_changes.backend.common.models.ration.RationModel
+import ru.fit_changes.backend.repo.ration.DbRationModelRequest
+import ru.fit_changes.backend.repo.ration.IRepoRation
+import java.util.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+abstract class RepoRationUpdateTest {
+    abstract val repo: IRepoRation
+
+    @Test
+    fun updateRationSuccess() {
+        val result = runBlocking { repo.update(DbRationModelRequest(updateRation)) }
+        assertTrue(result.isSuccess)
+        assertTrue(result.errors.isEmpty())
+        assertEquals(updateRation, result.result)
+        println(result.result?.caloriesNorm)
+    }
+
+    companion object : BaseInitRation() {
+        override val initObjects: List<RationModel> = listOf(createInitTestModel())
+        private val updateRation = initObjects.first().copy(
+            caloriesNorm = CaloriesModel(2200.0)
+        )
+    }
+}
