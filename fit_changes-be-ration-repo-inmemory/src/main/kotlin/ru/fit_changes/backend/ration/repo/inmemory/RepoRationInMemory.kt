4f294d848f3d7210f792fc678153ea5499d2fd43
KonstantinKhan <khan366kos@gmail.com>
2022-06-26 17:11:00 +0500

    Created RepoRationInMemory.kt

---
+++ fit_changes-be-ration-repo-inmemory/src/main/kotlin/ru/fit_changes/backend/ration/repo/inmemory/RepoRationInMemory.kt
@@ -0,0 +1,53 @@
+package ru.fit_changes.backend.ration.repo.inmemory
+
+import org.ehcache.Cache
+import org.ehcache.CacheManager
+import org.ehcache.config.builders.CacheConfigurationBuilder
+import org.ehcache.config.builders.CacheManagerBuilder
+import org.ehcache.config.builders.ExpiryPolicyBuilder
+import org.ehcache.config.builders.ResourcePoolsBuilder
+import ru.fit_changes.backend.common.models.ration.RationIdModel
+import ru.fit_changes.backend.common.models.ration.RationModel
+import ru.fit_changes.backend.repo.ration.DbRationModelRequest
+import ru.fit_changes.backend.repo.ration.DbRationResponse
+import ru.fit_changes.backend.repo.ration.IRepoRation
+import java.time.Duration
+import java.util.*
+
+class RepoRationInMemory : IRepoRation {
+
+    private val cache: Cache<String, RationRow> = let {
+        val cacheManager: CacheManager = CacheManagerBuilder
+            .newCacheManagerBuilder()
+            .build(true)
+
+        cacheManager.createCache(
+            "ration-cache",
+            CacheConfigurationBuilder
+                .newCacheConfigurationBuilder(
+                    String::class.java,
+                    RationRow::class.java,
+                    ResourcePoolsBuilder.heap(100)
+                )
+                .withExpiry(ExpiryPolicyBuilder.timeToIdleExpiration(Duration.ofMinutes(60)))
+                .build()
+        )
+    }
+
+    private fun save(item: RationModel): DbRationResponse {
+        val row = RationRow(item)
+        cache.put(row.rationId, row)
+        return DbRationResponse(
+            isSuccess = true,
+            result = row.toInternal()
+        )
+    }
+
+    override suspend fun create(req: DbRationModelRequest): DbRationResponse {
+        return save(
+            req.ration.copy(
+                rationId = RationIdModel(UUID.randomUUID())
+            )
+        )
+    }
+}
