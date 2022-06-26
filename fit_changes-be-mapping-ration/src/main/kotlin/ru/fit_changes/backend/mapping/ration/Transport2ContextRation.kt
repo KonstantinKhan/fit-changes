2a4e39dcc8f2896b85529ed78ab9bc198a04c6a0
KonstantinKhan <khan366kos@gmail.com>
2022-06-26 13:35:49 +0500

    Added operations.

--- fit_changes-be-mapping-ration/src/main/kotlin/ru/fit_changes/backend/mapping/ration/Transport2ContextRation.kt
+++ fit_changes-be-mapping-ration/src/main/kotlin/ru/fit_changes/backend/mapping/ration/Transport2ContextRation.kt
@@ -1,31 +1,37 @@
 package ru.fit_changes.backend.mapping.ration
 
 import ru.fit_changes.backend.common.context.BeContextRation
+import ru.fit_changes.backend.common.context.Operations
 import ru.fit_changes.backend.common.mappers.toModel
 import ru.fit_changes.backend.common.models.ration.*
 import ru.fit_changes.openapi.models.*
 
 fun BeContextRation.setQuery(query: CreateRationRequest) = apply {
+    operation = Operations.CREATE
     requestId = query.requestId ?: ""
     requestRation = query.createRation?.toModel() ?: RationModel()
 }
 
 fun BeContextRation.setQuery(query: ReadRationRequest) = apply {
+    operation = Operations.READ
     requestId = query.requestId ?: ""
     requestRationId = query.readRationId?.let { RationIdModel(it) } ?: RationIdModel.NONE
 }
 
 fun BeContextRation.setQuery(query: UpdateRationRequest) = apply {
+    operation = Operations.UPDATE
     requestId = query.requestId ?: ""
     requestRation = query.updateRation?.toModel() ?: RationModel()
 }
 
 fun BeContextRation.setQuery(query: DeleteRationRequest) = apply {
+    operation = Operations.DELETE
     requestId = query.requestId ?: ""
     requestRationId = query.deleteRationId?.let { RationIdModel(it) } ?: RationIdModel.NONE
 }
 
 fun BeContextRation.setQuery(query: SearchRationRequest) = apply {
+    operation = Operations.SEARCH
     requestId = query.requestId ?: ""
     requestRationFilter = query.query?.let { RationSearchFilter(searchString = it) } ?: RationSearchFilter()
 }
