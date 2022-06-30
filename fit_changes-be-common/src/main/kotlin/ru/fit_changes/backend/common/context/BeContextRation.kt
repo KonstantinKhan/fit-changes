e92ae186ffb36864e94e3994c1638f24355d5b43
KonstantinKhan <khan366kos@gmail.com>
2022-07-01 00:03:25 +0500

    Added config, workMode and rationRepo fields.

--- fit_changes-be-common/src/main/kotlin/ru/fit_changes/backend/common/context/BeContextRation.kt
+++ fit_changes-be-common/src/main/kotlin/ru/fit_changes/backend/common/context/BeContextRation.kt
@@ -2,17 +2,23 @@ package ru.fit_changes.backend.common.context
 
 import ru.fit_changes.backend.common.models.CommonErrorModel
 import ru.fit_changes.backend.common.models.IError
+import ru.fit_changes.backend.common.models.WorkMode
 import ru.fit_changes.backend.common.models.enums.StubCases
 import ru.fit_changes.backend.common.models.ration.RationIdModel
 import ru.fit_changes.backend.common.models.ration.RationModel
 import ru.fit_changes.backend.common.models.ration.RationSearchFilter
+import ru.fit_changes.backend.repo.ration.IRepoRation
 
 data class BeContextRation(
+    var config: RationContextConfig = RationContextConfig(),
+
     var requestId: String = "",
     var operation: Operations = Operations.NONE,
     var status: CorStatus = CorStatus.NONE,
+    var workMode: WorkMode = WorkMode.PROD,
 
     var stubCase: StubCases = StubCases.NONE,
+    var rationRepo: IRepoRation = IRepoRation.NONE,
 
     var requestRation: RationModel = RationModel(),
     var requestRationId: RationIdModel = RationIdModel.NONE,
