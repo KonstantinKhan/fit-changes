3c3121b96aaea9102937b2c04b4e2b3e01d29fe0
KonstantinKhan <khan366kos@gmail.com>
2022-06-30 22:59:26 +0500

    Added companion object NONE.

--- fit_changes-be-common/src/main/kotlin/ru/fit_changes/backend/repo/ration/IRepoRation.kt
+++ fit_changes-be-common/src/main/kotlin/ru/fit_changes/backend/repo/ration/IRepoRation.kt
@@ -6,4 +6,26 @@ interface IRepoRation {
     suspend fun update(req: DbRationModelRequest): DbRationResponse
     suspend fun delete(req: DbRationIdRequest): DbRationResponse
     suspend fun search(req: DbRationFilterRequest): DbRationsResponse
+
+    companion object NONE : IRepoRation {
+        override suspend fun create(req: DbRationModelRequest): DbRationResponse {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun read(req: DbRationIdRequest): DbRationResponse {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun update(req: DbRationModelRequest): DbRationResponse {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun delete(req: DbRationIdRequest): DbRationResponse {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun search(req: DbRationFilterRequest): DbRationsResponse {
+            TODO("Not yet implemented")
+        }
+    }
 }
