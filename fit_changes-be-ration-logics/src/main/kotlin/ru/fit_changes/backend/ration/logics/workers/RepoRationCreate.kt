4a43a0b4706168c786326fec33176b6b876cab2c
KonstantinKhan <khan366kos@gmail.com>
2022-07-09 13:23:21 +0500

    Created RepoRationCreate.kt

---
+++ fit_changes-be-ration-logics/src/main/kotlin/ru/fit_changes/backend/ration/logics/workers/RepoRationCreate.kt
@@ -0,0 +1,25 @@
+package ru.fit_changes.backend.ration.logics.workers
+
+import ru.fit_changes.backend.common.context.BeContextRation
+import ru.fit_changes.backend.common.context.CorStatus
+import ru.fit_changes.backend.repo.ration.DbRationModelRequest
+import ru.fit_changes.cor.CorChainDsl
+import ru.fit_changes.cor.worker
+
+fun CorChainDsl<BeContextRation>.repoRationCreate(title: String) = worker {
+    this.title = title
+    on {
+        status == CorStatus.RUNNING
+    }
+    handle {
+        val result = rationRepo.create(DbRationModelRequest(requestRation))
+        val resultValue = result.result
+        if (result.isSuccess && resultValue != null) {
+            responseRation = resultValue
+        } else {
+            result.errors.forEach {
+                addError(it)
+            }
+        }
+    }
+}
