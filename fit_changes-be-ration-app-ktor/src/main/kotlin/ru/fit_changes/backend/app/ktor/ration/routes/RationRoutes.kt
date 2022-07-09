8ccdbdeb335f634962c601d8a7888482b8cce906
KonstantinKhan <khan366kos@gmail.com>
2022-07-09 19:55:33 +0500

    Implemented read endpoint.

--- fit_changes-be-ration-app-ktor/src/main/kotlin/ru/fit_changes/backend/app/ktor/ration/routes/RationRoutes.kt
+++ fit_changes-be-ration-app-ktor/src/main/kotlin/ru/fit_changes/backend/app/ktor/ration/routes/RationRoutes.kt
@@ -3,6 +3,7 @@ package ru.fit_changes.backend.app.ktor.ration.routes
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import ru.fit_changes.backend.app.ktor.ration.controllers.createRation
+import ru.fit_changes.backend.app.ktor.ration.controllers.readRation
 import ru.fit_changes.backend.ration.service.RationService
 
 fun Application.rationRoutes(rationService: RationService) {
@@ -12,7 +13,7 @@ fun Application.rationRoutes(rationService: RationService) {
                 call.createRation(rationService)
             }
             post("read") {
-
+                call.readRation(rationService)
             }
             post("updated") {
 
