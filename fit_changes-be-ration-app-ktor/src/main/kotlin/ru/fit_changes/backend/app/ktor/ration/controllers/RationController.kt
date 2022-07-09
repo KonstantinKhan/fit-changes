5aa2c197fd4a948932566ddccd6f707d7216d131
KonstantinKhan <khan366kos@gmail.com>
2022-07-09 19:57:34 +0500

    Added readRation method.

--- fit_changes-be-ration-app-ktor/src/main/kotlin/ru/fit_changes/backend/app/ktor/ration/controllers/RationController.kt
+++ fit_changes-be-ration-app-ktor/src/main/kotlin/ru/fit_changes/backend/app/ktor/ration/controllers/RationController.kt
@@ -5,9 +5,17 @@ import ru.fit_changes.backend.app.ktor.ration.helpers.handleRoutes
 import ru.fit_changes.backend.ration.service.RationService
 import ru.fit_changes.openapi.models.CreateRationRequest
 import ru.fit_changes.openapi.models.CreateRationResponse
+import ru.fit_changes.openapi.models.ReadRationRequest
+import ru.fit_changes.openapi.models.ReadRationResponse
 
 suspend fun ApplicationCall.createRation(rationService: RationService) {
-    handleRoutes<CreateRationRequest, CreateRationResponse> {request ->
+    handleRoutes<CreateRationRequest, CreateRationResponse> { request ->
         rationService.createRation(this, request)
+    }
+}
+
+suspend fun ApplicationCall.readRation(rationService: RationService) {
+    handleRoutes<ReadRationRequest, ReadRationResponse> { request ->
+        rationService.readRation(this, request)
     }
 }
