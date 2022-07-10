ef97f76e4799d51f7642f11f47a85bc6a82f0903
KonstantinKhan <khan366kos@gmail.com>
2022-07-10 22:46:47 +0500

    Added ration service.

--- fit_changes-client-angular/src/app/ration/ration-page/ration-page.component.ts
+++ fit_changes-client-angular/src/app/ration/ration-page/ration-page.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Subscription} from "rxjs";
 import {Product} from "../../shared/interfaces/product";
+import {RationService} from "../../services/ration.service";
 
 @Component({
   selector: 'app-ration-page',
@@ -11,10 +12,10 @@ export class RationPageComponent implements OnInit {
   products: Product[] = [];
   subscriptions: Subscription[] = [];
 
-  constructor() {
+  constructor(private rationService: RationService) {
   }
 
   ngOnInit() {
-
+    this.rationService.searchProducts('2022-06-12T08:00:00.0Z').subscribe(rations => console.log(rations))
   }
 }
