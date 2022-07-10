7ee8e1626d00f500f0aef8458fbcd72e637166ba
KonstantinKhan <khan366kos@gmail.com>
2022-07-10 22:45:33 +0500

    Updated main module.

--- fit_changes-be-ration-app-ktor/src/main/kotlin/ru/fit_changes/backend/app/ktor/ration/Application.kt
+++ fit_changes-be-ration-app-ktor/src/main/kotlin/ru/fit_changes/backend/app/ktor/ration/Application.kt
@@ -2,24 +2,49 @@ package ru.fit_changes.backend.app.ktor.ration
 
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.SerializationFeature
+import io.ktor.http.*
 import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.cors.routing.*
 import ru.fit_changes.backend.app.ktor.ration.routes.rationRoutes
 import ru.fit_changes.backend.common.context.RationContextConfig
 import ru.fit_changes.backend.common.models.ration.RationIdModel
 import ru.fit_changes.backend.ration.logics.RationCrud
 import ru.fit_changes.backend.ration.service.RationService
 import ru.fit_changes.backend.utils.product.RATION_FILLED_MODEL
+import java.util.*
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 fun Application.module(
-    config: AppKtorConfig = AppKtorConfig()
+    config: AppKtorConfig = AppKtorConfig(
+        contextConfig = RationContextConfig(
+            repoRationTest = testRepo {
+                rations {
+                    ration {
+                        prototype = RATION_FILLED_MODEL.copy(
+                            rationId = RationIdModel(UUID.randomUUID().toString())
+                        )
+                    }
+                }
+            }
+        )
+    )
 ) {
+
+    install(CORS) {
+        allowHost("localhost:4200")
+        allowHeader(HttpHeaders.AccessControlAllowOrigin)
+        allowHeader(HttpHeaders.ContentType)
+//        allowHeader(HttpHeaders.Authorization)
+    }
+
     val rationService = RationService(crud = RationCrud(config.contextConfig))
     rationRoutes(rationService)
+
+
 }
 
 fun Application.contentNegotiation() {
