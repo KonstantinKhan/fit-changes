357a88aaf8cf56e39cd5e29b3562bfeb1e5a81d4
KonstantinKhan <khan366kos@gmail.com>
2022-07-10 15:42:45 +0500

    Added chooseDb and repoUpdateRation workers.

--- fit_changes-be-ration-logics/src/main/kotlin/ru/fit_changes/backend/ration/logics/chains/RationUpdate.kt
+++ fit_changes-be-ration-logics/src/main/kotlin/ru/fit_changes/backend/ration/logics/chains/RationUpdate.kt
@@ -5,11 +5,15 @@ import ru.fit_changes.backend.common.context.Operations
 import ru.fit_changes.backend.ration.logics.stubs.rationUpdateStub
 import ru.fit_changes.backend.ration.logics.workers.chainInitWorker
 import ru.fit_changes.backend.ration.logics.workers.checkOperationWorker
+import ru.fit_changes.backend.ration.logics.workers.chooseDb
+import ru.fit_changes.backend.ration.logics.workers.repoUpdateRation
 import ru.fit_changes.cor.ICorExecutor
 import ru.fit_changes.cor.chain
 
 object RationUpdate : ICorExecutor<BeContextRation> by chain<BeContextRation>({
     checkOperationWorker("Checking operation", Operations.UPDATE)
     chainInitWorker("Chain initialize")
+    chooseDb("Choose DB or STUB")
     rationUpdateStub("Processing of stub case UPDATE")
+    repoUpdateRation("Updated object in DB")
 }).build()
